457d6f678f3332d1e876ab206c3c646ea1235709
Maxime <maxime.bardy@studentfr.ch>
2025-12-09 11:43:37 +0100

    Implémentation base manette

--- code/exploration-rover/src/main/java/manette/model/ManetteModel.java
+++ code/exploration-rover/src/main/java/manette/model/ManetteModel.java
@@ -1,5 +1,103 @@
 package manette.model;
 
 public class ManetteModel {
-    // TODO: état de la manette (joysticks, boutons, modes)
+
+    public enum ModeVitesse {
+        LENTE,
+        NORMALE
+    }
+
+    private boolean connected;
+
+    private float leftX;
+    private float leftY;
+    private float rightX;
+    private float rightY;
+
+    private boolean buttonA;
+    private boolean buttonB;
+    private boolean buttonLB;
+    private boolean buttonRB;
+
+    private ModeVitesse modeVitesse = ModeVitesse.NORMALE;
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public void setConnected(boolean connected) {
+        this.connected = connected;
+    }
+
+    public float getLeftX() {
+        return leftX;
+    }
+
+    public void setLeftX(float leftX) {
+        this.leftX = leftX;
+    }
+
+    public float getLeftY() {
+        return leftY;
+    }
+
+    public void setLeftY(float leftY) {
+        this.leftY = leftY;
+    }
+
+    public float getRightX() {
+        return rightX;
+    }
+
+    public void setRightX(float rightX) {
+        this.rightX = rightX;
+    }
+
+    public float getRightY() {
+        return rightY;
+    }
+
+    public void setRightY(float rightY) {
+        this.rightY = rightY;
+    }
+
+    public boolean isButtonA() {
+        return buttonA;
+    }
+
+    public void setButtonA(boolean buttonA) {
+        this.buttonA = buttonA;
+    }
+
+    public boolean isButtonB() {
+        return buttonB;
+    }
+
+    public void setButtonB(boolean buttonB) {
+        this.buttonB = buttonB;
+    }
+
+    public boolean isButtonLB() {
+        return buttonLB;
+    }
+
+    public void setButtonLB(boolean buttonLB) {
+        this.buttonLB = buttonLB;
+    }
+
+    public boolean isButtonRB() {
+        return buttonRB;
+    }
+
+    public void setButtonRB(boolean buttonRB) {
+        this.buttonRB = buttonRB;
+    }
+
+    public ModeVitesse getModeVitesse() {
+        return modeVitesse;
+    }
+
+    public void setModeVitesse(ModeVitesse modeVitesse) {
+        this.modeVitesse = modeVitesse;
+    }
 }
