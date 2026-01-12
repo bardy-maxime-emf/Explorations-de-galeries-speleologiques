d147d9d879ac16f2b05ef1d3f51ea688a1c81fac
Maxime <maxime.bardy@studentfr.ch>
2026-01-12 15:29:19 +0100

    Encore

--- code/exploration-rover/src/main/java/common/RoverConfig.java
+++ code/exploration-rover/src/main/java/common/RoverConfig.java
@@ -1,13 +1,13 @@
 package common;
 
 public record RoverConfig(
-                String ip,
-                int port,
-                String serverName,
-                int motorHubPort,
-                int sonarHubPort,
-                int temperaturePort,
-                int lightHubPort,
-                int tofLeftHubPort,
-                int tofRightHubPort) {
+        String ip,
+        int port,
+        String serverName,
+        int motorHubPort,
+        int sonarHubPort,
+        int temperaturePort,
+        int lightHubPort,
+        int tofLeftHubPort,
+        int tofRightHubPort) {
 }
